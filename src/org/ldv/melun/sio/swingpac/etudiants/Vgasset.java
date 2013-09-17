b27c3b9e68c135d8f7c1c5d16b331160d57fbc3a
sio <sio@unassigned-hostname.unassigned-domain>
2013-09-17 09:17:21 +0200

    Pause, restart, fenetre et classe

---
+++ src/org/ldv/melun/sio/swingpac/etudiants/Vgasset.java
@@ -0,0 +1,26 @@
+package org.ldv.melun.sio.swingpac.etudiants;
+
+import java.awt.Color;
+
+import org.ldv.melun.sio.swingpac.Bidule;
+
+public class Vgasset extends Bidule {
+
+  
+  public Vgasset() {
+    super("Vgasset");
+    setBackground(Color.BLACK);    
+  }
+
+  @Override
+  public void doMove() {  
+    super.doMove();    
+  }
+
+  @Override
+  protected void doAfterImpactByOther() {
+    super.doAfterImpactByOther();
+  }
+
+  
+}
